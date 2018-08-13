79916f39cfe80c04535ef21e20d068de34edcdb5
Pedro Rocha <pebarrocha@hotmail.com>
2018-08-13 12:31:49 -0300

    MAIN + STYLE CSS

---
+++ view/main.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+	<head lang="pt-br">
+		<title>Site de personalização de camisetas </title>
+		<meta charset="pt-br">
+		<link rel ="stylesheet" type="text/css" href="css/main-estilo.css">
+		
+	</head>
+
+	<body>
+		<h1> Seja Bem vindo ao site de camisetas </h1>
+		<div class ="main">
+			<h2> Faça seu login </h2>
+			<form action="#" name="main" id="main-form"> 
+			<label>Login</label><br>
+			<label>E-mail:</label>
+			<input type="text" name="main-email" id="main-email"><br>
+			<label>Senha:</label>
+			<input type="password" name="main-password" id="main-password">
+			<input type="button" name="main-submit" id="main-submit" value="Logar"><br>
+			<a href="#">Não sou cadastrado</a>
+			</form>
+		</div>
+		
+		<div class ='main'>
+			<h1>Conheça os nossos produtos</h1>
+				<div class ="produto"> 
+
+
+				</div>
+
+		</div>
+
+	</body>
+	
+
+
+
+
+</html>
+
+
