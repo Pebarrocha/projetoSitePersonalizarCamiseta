d38515516dc104543b0934ea8c8295fef137efa2
Pedro Rocha <pebarrocha@hotmail.com>
2018-08-13 12:32:58 -0300

    ADM PAGE

---
+++ view/admVIEW.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Administração</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/adm-estilo.css">
+	<head>
+
+	<body>
+		<h1> Seja bem vindo a pádgina de administração </h1>
+		
+		<a href="#">Cadastrar Estampa</a>
+		<a href="#">Cadastrar Camiseta Pré-Definida</a>
+		<a href="#">Cadastrar Funcionário</a>
+		<a href="#">Cadastrar Itens de Personalização</a>
+	
+	</body>
+
+</html>
