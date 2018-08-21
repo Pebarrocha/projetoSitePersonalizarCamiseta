82171410d4d02e36ecda50dd1cf7d3ac7013f431
StormpeX <38654490+StormpeX@users.noreply.github.com>
2018-08-21 14:44:07 -0300

    Adicionei cadastro de cliente
    Sem mais
    
    Co-Authored-By: stormpex <stormpex@users.noreply.github.com>

---
+++ view/CadastroDeCliente.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Cadastro de cliente</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/adm-estilo.css">
+	<head>
+
+	<body>
+		<h1> Seja bem vindo ao cadastro de clientes </h1>
+		
+		<a href="#">Cadastrar Estampa</a>
+		<a href="#">Cadastrar Camiseta Pré-Definida</a>
+		<a href="#">Cadastrar Funcionário</a>
+		<a href="#">Cadastrar Itens de Personalização</a>
+	
+	</body>
+
+</html>
