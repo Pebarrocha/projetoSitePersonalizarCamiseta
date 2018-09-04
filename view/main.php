421f6a4a758f787612bfe126b234be36207ecfcb
Pedro Rocha <pebarrocha@hotmail.com>
2018-09-04 14:26:11 -0300

    Fix

--- view/main.php
+++ view/main.php
@@ -4,7 +4,7 @@
 	<head lang="pt-br">
 		<title>Site de personalização de camisetas </title>
 		<meta charset="pt-br">
-		<link rel ="stylesheet" type="text/css" href="css/main-estilo.css">
+		<link rel ="stylesheet" type="text/css" href="css/main-estilo.css?09032018">
 		
 	</head>
 
@@ -13,13 +13,25 @@
 			<p>Cabeçalho (Imagem do site)</p>
 		</header>
 
-		<nav>
-			<p>Barra de navegação</p>
+		
+
+		<nav class="navegacao">
+			<ul>
+				<li><a href="#">Camisetas Masculinas</a>
+				<li><a href="#">Camisetas Femininas</a>	
+				<li><a href="#">Contato</a>	
+				<li><a href="#">Sobre</a>		
+				<form id="pesquisa" class="pesquisa">
+					<input type="button" class="botao" value="Pesquisar">
+					<input type="text" name="pesquisa" id="pesquisa" placeholder="Pesquise aqui!"> 
+			    </form>			
+			</ul>
+
 		</nav>
 
 					
 
-		<section id="produtos">
+		<section class="produtos">
 			<article>
 				TesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTesteTeste
 			</article>
