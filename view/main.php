e3bf55b97d92d0781474abd42a6edd47895f648c
Pedro Rocha <pebarrocha@hotmail.com>
2018-09-04 22:22:21 -0300

    Alterando Estilos

--- view/main.php
+++ view/main.php
@@ -6,10 +6,13 @@
 		<meta charset="pt-br">
 		<link rel ="stylesheet" type="text/css" href="css/main-estilo.css">
 		
+		<style>
+		.mySlides {display:none;}
+		</style>
 	</head>
 
 	<body>
-
+	
 		<nav class="navegacao" >
 			<ul>				
 				<form class="pesquisa">
@@ -17,11 +20,11 @@
 					<input type="button" class="login" value="Minha Conta">
 					<input type="button" class="login" value="Entrar">
 						<div id="pesq">
-						<form id="pesquisa" >
-							<input type="button" class="botao" value="Pesquisar">
-							<input type="text" name="pesquisa" id="pesquisa" placeholder="Pesquise aqui!"> 
-			    		</form>
-					</div>
+							<form id="pesquisa" >
+								<input type="button" class="botao" value="Pesquisar">
+								<input type="text" name="pesquisa" id="pesquisa" placeholder="Pesquise aqui!"> 
+				    		</form>
+						</div>
 				</form>					
 			</ul>
 		</nav>
@@ -30,9 +33,23 @@
 		<header>
 			<p>Cabeçalho (Imagem do site)</p>
 		</header>
-
 		
+	<div class="slider-holder">
+        <span id="slider-image-1"></span>
+        <span id="slider-image-2"></span>
+     	 <span id="slider-image-3"></span>
+        <div class="image-holder">
+            <img src="imagens/camiseta1.jpg" class="slider-image" />
+            <img src="imagens/camiseta2.jpg" class="slider-image" />
 
+        </div>
+        <div class="button-holder">
+            <a href="#slider-image-1" class="slider-change"></a>
+            <a href="#slider-image-2" class="slider-change"></a>
+        
+        </div>
+    </div>
+		
 		<nav class="navegacao">
 			<ul>
 				<li><a href="#">Camisetas Masculinas</a>
@@ -61,14 +78,11 @@
 
 		</section>
 
-		<fotter>
-			<p>Rodapé</p>
-		</fotter>
 
 
 	</body>
 	
-
+			
 
 
 
