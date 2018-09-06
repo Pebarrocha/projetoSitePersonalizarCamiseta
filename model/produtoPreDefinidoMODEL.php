e1c803aca2da1813cd5951c593f1d29c61f21f5d
Pedro Rocha <pebarrocha@hotmail.com>
2018-09-06 13:52:04 -0300

    PEDRO - Testes e Valores

--- model/produtoPreDefinidoMODEL.php
+++ model/produtoPreDefinidoMODEL.php
@@ -35,14 +35,14 @@ Primeiro é estabelecida a conexão com o banco, logo após é criada a consulta
 
 	public function consultarProdutoPreDefinido($sql){
 		require_once 'conexaomysql.php';
-		echo '<script> alert("ENTROU NO CONSULTAR!") </script>';
+		echo '<script> alert("ENTROU NO CONSULTAR! MODEL") </script>';
 		$result = mysqli_query($conexao,$sql);	
 
 		return $result;	
 	}
 
 	public function alterarProdutoPreDefinido($nome, $descricao, $tamanho,$imagem,$sexo,$modelo,$preco){
-
+		echo '<script> alert("ENTROU NO ALTERAR! MODEL") </script>';
 		$sql = 'update camisetapredefinida set nome="' .$nome.  ' ", descricao="'. $descricao . '", tamanho = " '. $tamanho .' ",
 				imagem = "'. $imagem .'", sexo=" '. $sexo .' ", modelo = "'.$modelo.'", preco="'.$preco.'"';
 		$result = mysqli_query($conexao,$sql);		
