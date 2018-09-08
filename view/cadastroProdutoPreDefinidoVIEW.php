51657239bd3a2acb03755089a628fc80362f7e67
Pedro Rocha <pebarrocha@hotmail.com>
2018-09-08 00:40:13 -0300

    Pedro - arrumando linhas

--- view/cadastroProdutoPreDefinidoVIEW.php
+++ view/cadastroProdutoPreDefinidoVIEW.php
@@ -22,32 +22,28 @@ Processo('incluir');
 <link rel="stylesheet" type="text/css" href="css/predefinido.css?13082018">
 <title>Cadastro de Camisetas Pré-Definidas</title>
 </head>
-<body>		
-<!-- 
+<body>
+<!--
 Criação dos formularios para recebimento das informações digitadas pelo usuário
 O input do formulario (a entrada) é representada pelo seu "name", o valor do name do input será responsavel por enviar o valor do dado digitado pra o controller e depois para o banco de dados
--->			
+-->
 <div>
 <h1> Cadastro de produtos pré-definidos </h1>
 <form enctype="multipart/form-data" action="#" name="form" id="form" method="post">
-<label>Nome:</label> 
+<label>Nome:</label>
 <input type="text" name="nome" id="nome" placeholder="Nome da camiseta aqui!"> <br>
 <label>Descrição:</label> <br>
 <textarea name="descricao" id="descricao" rows="10" cols="30" placeholder="Digite a descrição da camiseta aqui!"></textarea><br>
 <label>Tamanho:</label> <select name="tamanho" id="tamanho">
 <option value="esq">Escolha um valor</option>
 <option value="PP">PP</option>
-<option value="P">P</option>	
-<option value="M">M</option>		
+<option value="P">P</option>
+<option value="M">M</option>
 <option value="G">G</option>
 <option value="GG">GG</option>
 </select><br>
 <label>Imagem: <input type="text" name="imagem" value="imagens/nomedaimagem.jpg">
-<!-- 
-<label>Imagem:</label>
-<input type="hidden" name="MAX_FILE_SIZE" value="99999999"/>
-<input type="file" name="imagem"><br>
--->
+
 <br>
 <label>Sexo:</label> <select name="sexo" id="sexo">
 <option value="masculino">Masculino</option>
@@ -60,7 +56,7 @@ O input do formulario (a entrada) é representada pelo seu "name", o valor do na
 <option value="polo">Polo Masculina</option>
 <option value="babylook">Baby Look</option>
 <option value="cropped">Cropped</option>
-</select><br>			
+</select><br>
 <label>Preço:</label>
 <input type="text" name="preco" id="preco" placeholder="Digite o preço da camiseta">
 <input type="button" value="Consultar" onclick="">
@@ -68,21 +64,21 @@ O input do formulario (a entrada) é representada pelo seu "name", o valor do na
 java script, que disparará os inputs dos forms para a página controller
 O segundo botão é invisivel, ele será ativado dentro da função, e o controller receberá ele para poder enfim liberar os inputs -->
 <input type="button" name="button" id="button" value="Cadastrar" onclick="submitForm()">
-<input type="hidden" name="ok" id="ok">	
+<input type="hidden" name="ok" id="ok">
 </div>
-		
+
 </form>
 <script type="text/javascript" src="../js/validacao.js"> </script>
-<!--Script (javascript) para submeter as informações resgatadas nos inputs 
+<!--Script (javascript) para submeter as informações resgatadas nos inputs
 Ele pega os ids 'ok' e 'form' e trabalha com eles. O elemento ok é passado a ser true, enquanto
 o elemento de id 'form' é submetido-->
 <script type="text/javascript">
-function submitForm(){	
-validaCadastro();			
-document.getElementById("ok").value = "true";					
+function submitForm(){
+validaCadastro();
+document.getElementById("ok").value = "true";
 document.getElementById("form").submit();
 //document.forms['form'].action = "../controller/produtoPreDefinidoController.php";
-}		
+}
 </script>
 </body>
 </html>
--- view/main.php
+++ view/main.php
@@ -22,110 +22,65 @@ $total = mysqli_num_rows($dados);
 //
 ?>
 
-
-
 <html>
-	<head lang="pt-br">
-		<title>Site de personalização de camisetas </title>
-		<meta charset="pt-br">
-		<link rel ="stylesheet" type="text/css" href="css/main-estilo.css?09062018">
-		
-		
-	</head>
+<head lang="pt-br">
+<title>Site de personalização de camisetas </title>
+<meta charset="pt-br">
+<link rel ="stylesheet" type="text/css" href="css/main-estilo.css?09062018">
+</head>
 
-	<body>
-	
-		<nav class="navegacao" >
-			<ul>				
-				<form class="pesquisa">
-					<input type="button" class="login" value="Carrinho de Compras">
-					<input type="button" class="login" value="Minha Conta">
-					<input type="button" class="login" value="Entrar">
-						<div id="pesq">
-							<form id="pesquisa" >
-								<input type="button" class="botao" value="Pesquisar">
-								<input type="text" name="pesquisa" id="pesquisa" placeholder="Pesquise aqui!"> 
-				    		</form>
-						</div>
-				</form>					
-			</ul>
-		</nav>
-	
-		
-		<header>
-			<p>Cabeçalho (Imagem do site)</p>
-		</header>
-	
-		
-		<nav class="navegacao">
-			<ul>
-				<li><a href="#">Camisetas Masculinas</a>
-				<li><a href="#">Camisetas Femininas</a>	
-				<li><a href="#">Contato</a>	
-				<li><a href="#">Sobre</a>		
-							
-			</ul>
+<body>
+<nav class="navegacao" >
+<ul>
+<form class="pesquisa">
+<input type="button" class="login" value="Carrinho de Compras">
+<input type="button" class="login" value="Minha Conta">
+<input type="button" class="login" value="Entrar">
+<div id="pesq">
+<form id="pesquisa" >
+<input type="button" class="botao" value="Pesquisar">
+<input type="text" name="pesquisa" id="pesquisa" placeholder="Pesquise aqui!">
+</form>
+</div>
+</form>
+</ul>
+</nav>
 
-		</nav>
 
-					
+<header>
+<p>Cabeçalho (Imagem do site)</p>
+</header>
 
-		<section class="produtos">		
-			
-			<?php
-			/*
-				echo 
-					"<table border=1>
-					<tr>
-					<th>Código</th>
-					<th>Nome</th>
-					<th>Descricao</th>
-					<th>Tamanho</th>
-					<th>Imagem</th>
-					<th>Sexo</th>
-					<th>Modelo</th>
-					<th>Foto Camiseta</th>
-					<th>Preco</th>
-					</tr>";
 
-					while($row = mysqli_fetch_array($dados)){
-						
-						echo "<tr>";
-						echo "<td>" . $row['codCamisetaPreDefinida'] . "</td>";
-						echo "<td>" . $row['nome'] . "</td>";
-						echo "<td>" . $row['descricao'] . "</td>";
-						echo "<td>" . $row['tamanho'] . "</td>";
-						echo "<td>" . $row['imagem'] . "</td>";
-						echo "<td>" . $row['sexo'] . "</td>";
-						echo "<td>" . $row['modelo'] . "</td>";
-						echo "<td>" . $row['FotoCamiseta_codFotoCamiseta'] . "</td>";
-						echo "<td>" . $row['preco'] . "</td>";		
-						echo "</tr>";			
-					}
-					echo "</table>";
-					*/
-			?>		
-			<?php
-				while ($row = mysqli_fetch_array($dados)){
-				echo "<article>";				
-					echo "<a href='#'>" . $row['nome'] . "</a>";
-					echo "<br>";
-					echo "<br>";
-					echo '<img src="'.$row['imagem'].'" width="90%"/>';
-					echo "<br>";
-					echo "<p>" . $row['descricao'] . "</p>";
-					echo "<br>";
-					echo "<p> Preço: R$ " . $row['preco'] . "</p>";
-					
-				echo "</article>";
-				}
-			?>
-		</section>
-	</body>
+<nav class="navegacao">
+<ul>
+<li><a href="#">Camisetas Masculinas</a>
+<li><a href="#">Camisetas Femininas</a>
+<li><a href="#">Contato</a>
+<li><a href="#">Sobre</a>
+</ul>
+</nav>
+
+<section class="produtos">
+
+
+<?php
+while ($row = mysqli_fetch_array($dados)){
+echo "<article>";
+echo "<a href='#'>" . $row['nome'] . "</a>";
+echo "<br>";
+echo "<br>";
+echo '<img src="'.$row['imagem'].'" width="90%"/>';
+echo "<br>";
+echo "<p>" . $row['descricao'] . "</p>";
+echo "<br>";
+echo "<p> Preço: R$ " . $row['preco'] . "</p>";
+echo "</article>";
+}
+?>
+</section>
+</body>
 </html>
-
-<?php 
-
+<?php
 mysqli_free_result($dados);
-
 ?>
